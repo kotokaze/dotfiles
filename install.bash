76c1ea489fee4c440a49a5c5db36451526638962
Kotokaze <62094392+Kotokaze@users.noreply.github.com>
2021-06-23 19:38:49 +0900

    :white_check_mark: Update

--- install.bash
+++ install.bash
@@ -11,24 +11,14 @@ sudo apt install -y \
 sudo locale-gen ja_JP ja_JP.UTF-8
 sudo update-locale LC_ALL=ja_JP.UTF-8 LANG=ja_JP.UTF-8
 
-if [! -d $HOME/.caches/fonts ]
-  mkdir -p ~/.cache/fonts
-
-wget -vO ~/.cache/fonts/FantasqueSansMono-Normal.zip https://github.com/belluzj/fantasque-sans/releases/latest/download/FantasqueSansMono-Normal.zip
-unzip ~/.cache/fonts/FantasqueSansMono-Normal.zip ~/.cache/FantasqueSansMono-Normal
-cp -r ~/.cache/fonts/FantasqueSansMono-Normal/* ~/.fonts
-
 ln -isv ~/.cache/dotfiles/src/git/gitmessage.txt .gitmessage.txt
 ln -isv .cache/dotfiles/src/git/gitconfig .gitconfig
 ln -isv ~/.cache/dotfile/src/nanorc .nanorc
 ln -isv ~/.cache/dotfile/src/vim/vimrc .vimrc
 
 echo 'export LANG=ja_JP.UTF-8' >> ~/.profile
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
 echo 'export USE_GIT_URI=git://' >> ~/.profile
-echo 'eval "$(ssh-agent)" 1>/dev/null 2>&1' >> ~/.profile
 echo 'eval "$(gh completion -s bash)"' >> ~/.aliases
-echo '\nif command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
 source ~/.bashrc
 
-curl https://pyenv.run | bash
+echo "Font download: https://github.com/belluzj/fantasque-sans/releases/latest/download/FantasqueSansMono-Normal.zip"
