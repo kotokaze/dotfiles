9fc2ab1bfef17a1678fb6cb609a761268f8e114b
Kotokaze <62094392+Kotokaze@users.noreply.github.com>
2021-06-25 18:01:12 +0000

    :white_check_mark: Update

--- install.bash
+++ install.bash
@@ -18,11 +18,15 @@ sudo update-locale LC_ALL=ja_JP.UTF-8 LANG=ja_JP.UTF-8
 cd ~
 ln -isv ~/.config/dotfiles/src/git/gitmessage.txt .gitmessage.txt
 ln -isv .config/dotfiles/src/git/gitconfig .gitconfig
-ln -isv ~/.config/dotfile/src/nanorc .nanorc
+ln -isv ~/.config/dotfile/src/nano/nanorc .nanorc
 ln -isv ~/.config/dotfile/src/vim/vimrc .vimrc
-
+FantasqueSansMono-Normal.zip
 echo 'export LANG=ja_JP.UTF-8' >> ~/.profile
 echo 'export USE_GIT_URI=git://' >> ~/.profile
+echo 'eval "$(gh completion -s bash)"' >> ~/.bash_profile
+echo 'eval "$(ssh-agent)"' >> ~/.bash_profile
+echo 'export "GPG_TTY"=$(tty)' >> ~/.bash_profile
 source ~/.bashrc
 
-wget https://github.com/belluzj/fantasque-sans/releases/latest/download/FantasqueSansMono-Normal.zip
+curl https://raw.githubusercontent.com/riobard/bash-powerline/master/bash-powerline.sh > ~/.bash-powerline.sh
+curl https://github.com/belluzj/fantasque-sans/releases/latest/download/FantasqueSansMono-Normal.zip > ~/.cache/FantasqueSansMono-Normal.zip
